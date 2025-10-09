87be783b4a4d9fc603a6ecfac3cf3c9c4a3ec65e
Zan-0xy <ahmadsan272@gmail.com>
2025-10-09 22:09:30 +0700

    Update alyaseen.py

--- alyaseen.py
+++ alyaseen.py
@@ -90,7 +90,7 @@ def countdown(t):
 
 # Validasi URL dan Parsing Target
 def get_target(url):
-    if not valid
+    if not valid:
     ators.url(url):
         log_attack_status(f"URL tidak valid: {url}", level='error')
         raise ValueError(f"URL tidak valid: {url}")
